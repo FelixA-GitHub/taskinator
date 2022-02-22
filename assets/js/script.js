9f25e31c5b2f63ed81b1851a1731948c6219cefc
Your Name <felix.acevedo.tech@gmail.com>
2022-02-22 14:37:18 -0500

    adding event.preventDefault()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-// targets the button's id and the ul's id
-var buttonEl = document.querySelector("#save-task");
+// targets the forms's id and the ul's id
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to create a new li, add css styling, add text, and append it to the ul
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   };
-  //every time button is clicked, it will dynamically add a new li item in the ul using createTaskHandler function from above
-  buttonEl.addEventListener("click", createTaskHandler);
+  //every time button is clicked with a value of submit, it will dynamically add a new li item in the ul using createTaskHandler function from above
+  formEl.addEventListener("submit", createTaskHandler);

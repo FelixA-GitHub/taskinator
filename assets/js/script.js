3d86f5e8fe3f27a069e161903a1a436e1c47896c
Your Name <felix.acevedo.tech@gmail.com>
2022-02-22 15:07:41 -0500

    added div element to li item

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // function to create a new li, add css styling, add text, and append it to the ul
 var createTaskHandler = function(event) {
 
-    event.preventDefault();
+    event.preventDefault(); //prevents browser's default behavior
+    var taskNameInput = document.querySelector("input[name='task-name']").value;//[] used because we are selecting the input HTML element that has a name attribute set to task-name
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
+    var listItemEl = document.createElement("li");//creates new li item
+    listItemEl.className = "task-item"; //applies task-item class styling
+
+    var taskInfoEl = document.createElement("div");// create div to hold task info and add to list item
+    taskInfoEl.className = "task-info";//applies task-item class styling
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);// add entire list item to list
+
+};
   //every time button is clicked with a value of submit, it will dynamically add a new li item in the ul using createTaskHandler function from above
   formEl.addEventListener("submit", createTaskHandler);

957045da3598e689f5da8b61f8f6e961a1575bb3
Your Name <felix.acevedo.tech@gmail.com>
2022-02-22 15:18:43 -0500

    refactoring js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,36 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to create a new li, add css styling, add text, and append it to the ul
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
 
     event.preventDefault(); //prevents browser's default behavior
     var taskNameInput = document.querySelector("input[name='task-name']").value;//[] used because we are selecting the input HTML element that has a name attribute set to task-name
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) { 
+    
     var listItemEl = document.createElement("li");//creates new li item
     listItemEl.className = "task-item"; //applies task-item class styling
 
     var taskInfoEl = document.createElement("div");// create div to hold task info and add to list item
     taskInfoEl.className = "task-info";//applies task-item class styling
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
     listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);// add entire list item to list
-
 };
-  //every time button is clicked with a value of submit, it will dynamically add a new li item in the ul using createTaskHandler function from above
-  formEl.addEventListener("submit", createTaskHandler);
+
+  //every time button is clicked with a value of submit, it will dynamically add a new li item in the ul using taskFormHandler function from above
+  formEl.addEventListener("submit", taskFormHandler);
